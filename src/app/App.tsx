ef392359f0477f5d08a31666e5d54b048c9a3a77
Koiava Givi <111989167+sohumelivar@users.noreply.github.com>
2023-11-20 17:42:42 +0200

    svg_loader, file_loader, created Button UI kit

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,5 +1,4 @@
 import './styles/index.scss';
-import { Link } from 'react-router-dom';
 import { classNames } from 'shared/lib/classNames/classNames';
 import { useTheme } from 'app/providers/ThemeProvider';
 import { AppRouter } from './providers/router';
@@ -7,13 +6,12 @@ import { Navbar } from 'widgets/Navbar';
 
 const App = () => {
 
-    const { theme, toggleTheme } = useTheme();
+    const { theme } = useTheme();
 
     return (
         <div className={classNames('app', {}, [theme])}>
             <Navbar />
             <AppRouter />
-            <button onClick={toggleTheme}>TOGGLE</button>
         </div>
     )
 };
