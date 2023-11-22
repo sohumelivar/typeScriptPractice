2f85561ded5e4257d434b82cbdb39e4216fb6c3b
Koiava Givi <111989167+sohumelivar@users.noreply.github.com>
2023-11-22 14:28:27 +0200

    added i18n, define plugin

--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,16 @@ import { render } from "react-dom";
 import App from "./app/App";
 import { BrowserRouter } from "react-router-dom";
 import { ThemeProvider } from "app/providers/ThemeProvider";
+import './shared/config/i18n/i18n';
+import { createRoot } from 'react-dom/client';
 
-render(
+const container = document.getElementById('root');
+const root = createRoot(container);
+
+root.render(
     <BrowserRouter>
         <ThemeProvider>
             <App />
         </ThemeProvider>
     </BrowserRouter>,
-    document.getElementById('root')
 )
