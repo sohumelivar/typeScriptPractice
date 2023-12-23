c521cfb354bed1de2be47686cc98879735d67b76
Koiava Givi <111989167+sohumelivar@users.noreply.github.com>
2023-12-23 17:54:17 +0200

    added first features, auth by username

--- src/shared/ui/Modal/Modal.tsx
+++ src/shared/ui/Modal/Modal.tsx
@@ -9,6 +9,7 @@ interface ModalProps {
    children?: ReactNode;
    isOpen?: boolean;
    onClose?: () => void;
+   lazy?: boolean;
 };
 
 const ANIMATION_DELAY = 300;
@@ -19,11 +20,19 @@ export const Modal = (props: ModalProps) => {
         children,
         isOpen,
         onClose,
+        lazy
     } = props;
 
     const [isClosing, setIsClosing] = useState(false);
+    const [isMounted, setIsMounted] = useState(false);
     const timeRef = useRef<ReturnType<typeof setTimeout>>();
     const { theme } = useTheme();
+
+    useEffect(() => {
+        if (isOpen) {
+            setIsMounted(true);
+        }
+    }, [isOpen])
 
     const closeHandler = useCallback(() => {
         if (onClose) {
@@ -59,6 +68,10 @@ export const Modal = (props: ModalProps) => {
         [cls.opened]: isOpen,
         [cls.isClosing]: isClosing,
     };
+
+    if (lazy && !isMounted) {
+        return null;
+    }
 
     return (
         <Portal>
